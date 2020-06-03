8f17628f22a4f7aa4b86ce5b5e96ea32087c0214
dmgcsilva <dmgc.silva@campus.fct.unl.pt>
2020-06-03 20:04:26 +0100

    Added agnostic features extraction functions

--- Code/ImageProcessor.py
+++ Code/ImageProcessor.py
@@ -28,47 +28,26 @@ class ImageProcessor:
         self.vgg_block3 = []
 
 
-        self.image_names = np.load(FILES_DIR + "image_names.npz")["names"]
+        image_names = np.load(FILES_DIR + "image_names.npz")["names"]
 
         if update:
             if not os.path.exists(FILES_DIR):
                 os.makedirs(FILES_DIR)
 
-
             # Extracting HoC
-            for img_name in self.image_names:
-                self.colors.append(self.extract_img_hoc(img_name))
-
-            self.colors = np.array(self.colors)
-            np.savez('{}.npz'.format(FILES_DIR + "hoc_matrix"), hoc=self.colors)
+            self.extract_feature(image_names, "hoc_matrix", self.extract_img_hoc)
 
             # Extracting HoG
-            for img_name in self.image_names:
-                self.grads.append(self.extract_img_hog(img_name))
-
-            self.grads = np.array(self.grads)
-            np.savez('{}.npz'.format(FILES_DIR + "hog_matrix"), hog=self.grads)
+            self.extract_feature(image_names, "hog_matrix", self.extract_img_hog)
 
             # Extracting VGG16_block1
-            for img_name in self.image_names:
-                self.vgg_block1.append(self.extract_img_vggblock(img_name, "block1_pool"))
-
-            self.vgg_block1 = np.array(self.vgg_block1)
-            np.savez('{}.npz'.format(FILES_DIR + "vgg16_block1_matrix"), b1=self.vgg_block1)
+            self.extract_vgg_feature(image_names, "vgg16_block1_matrix", "block1_pool")
 
             # Extracting VGG16_block2
-            for img_name in self.image_names:
-                self.vgg_block2.append(self.extract_img_vggblock(img_name, "block2_pool"))
-
-            self.vgg_block2 = np.array(self.vgg_block2)
-            np.savez('{}.npz'.format(FILES_DIR + "vgg16_block2_matrix"), b2=self.vgg_block2)
+            self.extract_vgg_feature(image_names, "vgg16_block2_matrix", "block2_pool")
 
             # Extracting VGG16_block3
-            for img_name in self.image_names:
-                self.vgg_block3.append(self.extract_img_vggblock(img_name, "block3_pool"))
-
-            self.vgg_block3 = np.array(self.vgg_block3)
-            np.savez('{}.npz'.format(FILES_DIR + "vgg16_block3_matrix"), b3=self.vgg_block3)
+            self.extract_vgg_feature(image_names, "vgg16_block3_matrix", "block3_pool")
 
         else:
             # Reading feature matrices from files
@@ -97,11 +76,11 @@ class ImageProcessor:
         grad_feat = np.squeeze(normalize(grad_hist.reshape(1, -1), norm="l2"))
         return grad_feat
 
-    def extract_img_vggblock(self, img_name, block_name):
+    def extract_img_vggblock(self, img_name, layer_name):
         img = dm.get_single_img(img_name)
         img = self.center_crop_image(img, size=224)
 
-        vgg16 = fe.vgg16_layer(img, layer=block_name)
+        vgg16 = fe.vgg16_layer(img, layer=layer_name)
         return vgg16
 
     def extract_feature(self, img_names, npz_name, function):
@@ -111,6 +90,18 @@ class ImageProcessor:
 
         features = np.array(features)
         np.savez('{}.npz'.format(FILES_DIR + npz_name), feature=features)
+
+    def extract_vgg_feature(self, img_names, npz_name, layer_name):
+        features = []
+        for img_name in img_names:
+            features.append(self.extract_img_vggblock(img_name, layer_name))
+
+        features = np.array(features)
+        np.savez('{}.npz'.format(FILES_DIR + npz_name), feature=features)
+
+    @staticmethod
+    def load_feature(npz_name):
+        return np.load(FILES_DIR + npz_name + ".npz")["features"]
 
     @staticmethod
     def center_crop_image(im, size=224):
